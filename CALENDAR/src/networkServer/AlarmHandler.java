1a1f929fdb7a5e4a8a222a99ab4bcaeac1d5da2a
Birg <btopphol@hotmail.com>
2014-03-20 16:42:56 +0100

    Added removeInvitation to ServerRequest and RequestHandler. Did a fev minor changes to AlarmHandler and Server.

--- CALENDAR/src/networkServer/AlarmHandler.java
+++ CALENDAR/src/networkServer/AlarmHandler.java
@@ -1,7 +1,9 @@
 package networkServer;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -12,21 +14,37 @@ import model.Invitation;
 public class AlarmHandler {
 	
 	private Timer timer;
+	HashMap<Invitation, Timer> timers;
 	private EmailHandler emailHandler;
 
 	
 	public AlarmHandler () {
-		timer = new Timer();
 		emailHandler = new EmailHandler();
+		timers = new HashMap<Invitation, Timer>();
 	}
 	
 	public void setAlarm (Invitation invitation){
+		cancelOldTimer(invitation);
+		
+		timer = new Timer();
 		Calendar calendar = Calendar.getInstance();
 		calendar.setTimeInMillis(invitation.getAlarmTime());
 		Date alarmTime = calendar.getTime();
 		timer.schedule(new AlarmTask(invitation.getAppointment(), invitation.getEmployee()), alarmTime);
+		
+		timers.put(invitation, timer);
+		
+		System.out.println("AlarmHandler: Alarm set!");
 	}
 	
+	private void cancelOldTimer(Invitation invitation) {
+		if (timers.containsKey(invitation)){
+			timers.get(invitation).cancel();
+			timers.remove(invitation);
+			System.out.println("AlarmHandler: Old alarm removed!");
+		}
+	}
+
 	class AlarmTask extends TimerTask{
 		private Appointment appointment;
 		private Employee employee;
--- CALENDAR/src/networkServer/ServerRequest.java
+++ CALENDAR/src/networkServer/ServerRequest.java
@@ -59,6 +59,7 @@ public class ServerRequest {
 			case "SET_APPOINTMENT_AS_DELETED": return setDeletedAppointmentByID((int) packet.getObject(0));
 //			Remove
 			case "REMOVE_APPOINTMENT": return removeAppointmentByID((int) packet.getObject(0));
+			case "REMOVE_INVITATION": return removeInvitationByID((int) packet.getObject(0));
 //			Email
 			case "SEND_EMAIL": return sendEmail((String) packet.getObject(0), (String) packet.getObject(1));
 
@@ -297,6 +298,17 @@ public class ServerRequest {
 			System.out.println("ServerRequest: removeAppointmentByID failed!");
 			e.printStackTrace();
 			return new Packet("ERROR", "ServerRequest: removeAppointmentByID failed!", e);
+		}
+	}
+	private Packet removeInvitationByID(int invitationID) {
+		try {
+			db.removeInvitationByID(invitationID);
+			return new Packet("INVITATION_REMOVED");
+		} 
+		catch (Exception e) {
+			System.out.println("ServerRequest: removeInvitationByID failed!");
+			e.printStackTrace();
+			return new Packet("ERROR", "ServerRequest: removeInvitationByID failed!", e);
 		}
 	}
 //	Email
